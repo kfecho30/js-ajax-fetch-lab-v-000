ba6807fec9ed5cb81070cbdd7e5d12961563c3f3
Kelsey Fecho <kfecho30@gmail.com>
2018-06-26 17:19:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,13 +18,10 @@ function forkRepo() {
     headers: {
       Authorization: `token ${getToken()}`
     }
-  })
+    }).then(res => console.log(res));
+  }
 }
 
 function getToken() {
-  const token = '3c1cd86a8d162ead8772f0862a330e5f20881d40'
-
-  //change to your token to run in browser, but set
-  //back to '' before committing so all tests pass
   return ''
 }
