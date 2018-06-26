5bbbbc0f9af11c610eb73d74e200867ad75b5161
Kelsey Fecho <kfecho30@gmail.com>
2018-06-26 17:42:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ function showResults(json) {
 function forkRepo() {
   const repo = 'learn-co-curriculum/javascript-fetch-lab'
   const token = getToken()
-  
+
   fetch(`https://api.github.com/repos/${repo}/forks`, {
     method: /post/,
     headers: {
